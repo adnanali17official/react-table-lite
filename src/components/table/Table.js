a6059b775995ae01e937cdaab3fa4fe192a5a682
Daniyal45 <daniyal_09.2005@hotmail.com>
2020-07-30 17:38:11 +0500

    Major functionalities and styling

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,30 +1,84 @@
 import React from "react";
+import "./Table.css";
 
 export default class Table extends React.Component {
   constructor(props) {
-    super(props);
-
+    super(props);    
     this.state = {};
   }
 
+
+  TableHeader = () =>{
+    let {
+        header , 
+        headerStyle
+    } = this.props;    
+    return(
+      <thead className="react-table-lite-header" style={headerStyle}>
+        <tr>
+          {header.length?
+            header.map((heading,index)=>(
+              <th key={index}>
+                  {heading}
+              </th>
+            ))
+            :
+          ""
+          }
+        </tr>
+      </thead>
+    )
+  }
+
+  TableData = () =>{
+    let { 
+      data, 
+      header,
+      limit,
+      rowStyle,
+      dataStyle
+     } = this.props;  
+
+    return(
+      <tbody>
+        {data.length?
+            data.map((data_row,index)=>
+              {
+                if(index!==limit){
+                  return(
+                  <tr 
+                    key={index} 
+                    style={rowStyle}
+                    className="react-table-lite-row"
+                  >  
+                      {
+                        header.map((header_key,index)=>(
+                          <td 
+                            key={index} 
+                            style={dataStyle}
+                          >
+                              {data_row[header_key]}
+                          </td>
+                        ))                    
+                      }                      
+                  </tr>     
+                  )      
+                }        
+              })
+            :
+          ""
+        }        
+      </tbody>
+    )
+  }
   render() {
     return (
-      <div>
-        <table>
-          <tr>
-            <th>ID</th>
-            <th>Job</th>
-          </tr>
-          <tr>
-            <td>1</td>
-            <td>Dev</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Tester</td>
-          </tr>
+      <div className="react-table-lite-container" style={this.props.containerStyle}>
+        <table cellspacing={0} className="react-table-lite-main" style={this.props.tableStyle}>
+          {this.TableHeader()}
+          {this.TableData()}
         </table>
-      </div>
+      </div>  
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Table from "./components/table/Table";
 
-ReactDOM.render(<Table />, document.getElementById("root"));
+ReactDOM.render(
+    <Table 
+        // limit={10}              
+        // header={["name","age","phone","email","job"]}
+        // data={
+        //     [
+        //         {id:"232", name:"ABC", age:"22", phone:"+232-1230110", email:"abc@hotmail.com"},
+        //         {name:"XYZ", age:"25", email:"abc@hotmail.com", phone:"+230-1230110", job:"developer" },
+        //         {email:"rew@hotmail.com", name:"REW", age:"21", phone:"+250-1230110", role:"developer" },
+        //     ]
+        // }
+    />, 
+    document.getElementById("root")
+);
